65a26776c726e65dd01b058b361f4d71ed4b84ae
Annita Maniri <annitk@gmail.com>
2023-01-25 18:48:45 +0200

    add Example005_If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter user name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "annita")
+{
+    Console.WriteLine("Glad to see you Annita!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
