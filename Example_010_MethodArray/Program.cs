54e7894f1518a7040d1bb84295c62a443698a148
Annita Maniri <annitk@gmail.com>
2023-01-28 14:02:40 +0200

    add Example_010_MethodArray найти элемент массива

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный find
+
+int[] array = { 1, 6, 154, 48, 0, 89, 74, 32, 1, 654, 87, 61, 40 };
+
+int n = array.Length;
+int find = 1;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+Console.WriteLine("The End");
