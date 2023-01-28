b8593e5fde675e10f0d5b99dac75e0fcd28cf89b
Annita Maniri <annitk@gmail.com>
2023-01-28 15:07:09 +0200

    add Example_011_ArrayLibrary заполнение и печать массива

---
+++ Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿// Метод заполнения и печати массива
+
+void FillArray(int[] collection) // метод, который ничего не возвращает
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection2)
+{
+    int count = collection2.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(collection2[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // если элемента нет, то по умолчанию возвращается -1
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array,14);
+Console.WriteLine(pos);
